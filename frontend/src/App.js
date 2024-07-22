6ef31142175cfe4420c60d087316882571c7f3c4
root <root@LAPTOP-KKC7KSCT.localdomain>
2024-07-22 19:52:43 -0400

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import ScannerPage from './pages/ScannerPage';
-import NotFound from './pages/NotFound';
+import Scanner from './components/Scanner';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/scanner" component={ScannerPage} />
-          <Route component={NotFound} />
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <Scanner />
+    </div>
   );
 }
 
---
+++ frontend/src/components/Scanner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Scanner = () => {
+  return (
+    <div>
+      <h1>Barcode Scanner</h1>
+      <p>Scan a barcode here.</p>
+      {/* Implement barcode scanning functionality */}
+    </div>
+  );
+}
+
+export default Scanner;
